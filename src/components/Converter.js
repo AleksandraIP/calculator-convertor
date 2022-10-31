0541a83150fe6bd00c01c15b8925fdbc09dfe69a
AleksandraIP <85641733+AleksandraIP@users.noreply.github.com>
2022-10-31 11:40:56 +0100

    width

--- src/components/Converter.js
+++ src/components/Converter.js
@@ -11,19 +11,51 @@ function Converter(){
     const first = useRef();
     const second = useRef();
     function convert(){
-        if (first.current.value === 'Meters'){
+        if (first.current.value === 'Meter'){
             switch (second.current.value){
-                case 'Centimeters': setResult(input*100);
-                break;
-                case 'Meters': setResult(input);
-                break;
+                case 'Millimeter': setResult(input*1000);
+                    break;
+                case 'Centimeter': setResult(input*100);
+                    break;
+                case 'Meter': setResult(input);
+                    break;
+                case 'KiloMeter': setResult(input/1000);
+                    break;
             }
         }
-        if (first.current.value === 'Centimeters'){
+        if (first.current.value === 'Centimeter'){
             switch (second.current.value){
-                case 'Centimeters': setResult(input);
+                case 'Millimeter': setResult(input*10);
                     break;
-                case 'Meters': setResult(input/100);
+                case 'Centimeter': setResult(input);
+                    break;
+                case 'Meter': setResult(input/100);
+                    break;
+                case 'KiloMeter': setResult(input/100000);
+                    break;
+            }
+        }
+        if (first.current.value === 'KiloMeter'){
+            switch (second.current.value){
+                case 'Millimeter': setResult(input*1000000);
+                    break;
+                case 'Centimeter': setResult(input*100000);
+                    break;
+                case 'Meter': setResult(input*1000);
+                    break;
+                case 'KiloMeter': setResult(input);
+                    break;
+            }
+        }
+        if (first.current.value === 'Millimeter'){
+            switch (second.current.value){
+                case 'Millimeter': setResult(input);
+                    break;
+                case 'Centimeter': setResult(input/10);
+                    break;
+                case 'Meter': setResult(input/1000);
+                    break;
+                case 'KiloMeter': setResult(input/1000000);
                     break;
             }
         }
@@ -33,16 +65,20 @@ function Converter(){
             <Text>{result}</Text>
             <Input onChange={(e)=>{setInput(e.target.value)}} w='50%' type='number'/>
             <Flex gap='15px'>
-                <Select ref={first} size='md' w='90%'>
-                    <option value='Centimeters'>Centimeters</option>
-                    <option value='Meters'>Meters</option>
+                <Select ref={first} size='md' w='100%'>
+                    <option value='Millimeter'>Millimeter</option>
+                    <option value='Centimeter'>Centimeter</option>
+                    <option value='Meter'>Meter</option>
+                    <option value='KiloMeter'>KiloMeter</option>
                 </Select>
             </Flex>
 
             <Flex>
-                <Select ref={second} size='md' w='90%'>
-                    <option value='Centimeters'>Centimeters</option>
-                    <option value='Meters'>Meters</option>
+                <Select ref={second} size='md' w='100%'>
+                    <option value='Millimeter'>Millimeter</option>
+                    <option value='Centimeter'>Centimeter</option>
+                    <option value='Meter'>Meter</option>
+                    <option value='KiloMeter'>KiloMeter</option>
                 </Select>
             </Flex>
             <Button onClick={()=>{convert()}}>Convert</Button>
